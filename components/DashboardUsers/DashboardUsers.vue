fb4bc0dc2af5f5b6178c6ff5a4c0a8318a12754a
Anton Lovrić <antonlovri81@gmail.com>
2022-07-21 23:54:32 +0200

    create users table on dashboard implemented user delete function

---
+++ components/DashboardUsers/DashboardUsers.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="px-4">
+        <div class="mx-auto max-w-[90%] overflow-x-scroll">
+            <h3 class="mb-2">Korisnici</h3>
+            <va-data-table
+                class="text-white"
+                striped
+                :items="users.userCollection"
+                :columns="columns"
+                selectable
+                v-model="selectedItems"
+                @selection-change="selectHandler"
+                select-mode="multiple"
+            >
+            </va-data-table>
+        </div>
+        <div class="ml-16 inline-flex items-center gap-4 justify-start mt-5">
+            <span>Šifre označenih računa: {{ items.ids.toString() }}</span>
+            <va-button @click="removeHandler" color="danger" icon="delete_forever"
+                >Obriši</va-button
+            >
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~~/stores/user';
+
+const errorStatus = ref(null);
+const porukaBrisanje = ref('Sigurni ste da želite obrisati korisnika? ');
+const { init } = useToast();
+const config = useRuntimeConfig();
+const users = reactive({ userCollection: [] });
+const userData = useUserStore();
+const selectedItems = ref([]);
+const items = reactive({ ids: [] });
+
+const selectHandler = (prop) => {
+    items.ids = selectedItems.value.map((item) => item['id']);
+};
+
+const removeHandler = async () => {
+    const response = await useFetch(`${config.API_BASE_URL}/users/deleteUsers`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${userData.token}`,
+        },
+        body: {
+            ids: items.ids,
+        },
+        async onResponseError({ response }) {
+            errorStatus.value = response.status;
+            init({
+                title: 'Odjava',
+                position: 'top-right',
+                message: 'Greška prilikom brisanja!',
+                color: 'danger',
+                duration: 5000,
+            });
+        },
+    });
+    if (response.status === 204) {
+        init({
+            title: 'Odjava',
+            position: 'top-right',
+            message: 'Korisnici uspješno obrisani!',
+            color: 'danger',
+            duration: 5000,
+        });
+    }
+};
+
+const response = await useFetch(`${config.API_BASE_URL}/users`, {
+    method: 'GET',
+    headers: {
+        Authorization: `Bearer ${userData.token}`,
+    },
+    async onResponseError({ response }) {
+        errorStatus.value = response.status;
+        init({
+            title: 'Odjava',
+            position: 'top-right',
+            message: 'Greška prilikom odjave!',
+            color: 'danger',
+            duration: 5000,
+        });
+    },
+    initialCache: false,
+});
+
+users.userCollection = response.data.value?.data || [];
+const columns =
+    [
+        { key: 'id', name: 'id', label: 'ID' },
+        { key: 'username', name: 'username', label: 'Korisničko Ime' },
+        { key: 'first_name', name: 'first_name', label: 'Ime' },
+        { key: 'last_name', name: 'last_name', label: 'Prezime' },
+        { key: 'email', name: 'email', label: 'E-Mail' },
+    ] || [];
+</script>
+
+<style>
+div {
+    --va-data-table-thead-color: #fff;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <the-header />
+        <admin-dashboard v-if="userData.type == 2" />
+        <the-footer />
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~~/stores/user';
+
+const userData = useUserStore();
+</script>
