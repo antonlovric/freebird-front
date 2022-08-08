f6888599d920eefef345cdea344cb3eee32cb905
Anton Lovrić <antonlovri81@gmail.com>
2022-08-08 15:36:32 +0200

    revert changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
     <div>
         <the-header />
         <homepage-hero />
-        <!-- <catalogue-section />
-        <news-section /> -->
+        <catalogue-section />
+        <news-section />
         <the-footer />
     </div>
 </template>
