d8adef8c89be7ef12f7d0e38a8426a4220a55f19
Anton Lovrić <antonlovri81@gmail.com>
2023-06-26 01:24:05 +0200

    revert: maintenance tag

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 import { useUserStore } from '~~/stores/user';
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    return navigateTo('/')
-    const userData = useUserStore();
-    if (userData.token === '') return navigateTo('/login');
+  // return navigateTo('/')
+  const userData = useUserStore();
+  if (userData.token === '') return navigateTo('/login');
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-       <!-- <the-header />
-        <homepage-hero />
-        <catalogue-section />
-        <news-section />
-        <the-footer />   -->
-        <h1 class="text-center text-6xl">Under Maintenance :)</h1>
-    </div>
+  <div>
+    <the-header />
+    <homepage-hero />
+    <catalogue-section />
+    <news-section />
+    <the-footer />
+  </div>
 </template>
 
 <script setup>
 useHead({
-    title: 'Free Bird Music',
-    meta: [
-        {
-            name: 'description',
-            content: 'Domaća stranica Free Bird Music glazbenog dućana.',
-        },
-    ],
-    htmlAttrs: {
-        lang: 'hr',
+  title: 'Free Bird Music',
+  meta: [
+    {
+      name: 'description',
+      content: 'Domaća stranica Free Bird Music glazbenog dućana.',
     },
+  ],
+  htmlAttrs: {
+    lang: 'hr',
+  },
 });
 </script>
