9548d1411d7831f19e39992706774319232ef578
Anton Lovrić <antonlovri81@gmail.com>
2023-07-17 17:55:57 +0200

    bugfix: fixed typo

--- components/DashboardProducts/DashboardProducts.vue
+++ components/DashboardProducts/DashboardProducts.vue
@@ -1,69 +1,69 @@
 <template>
-    <div class="px-4 my-5">
-        <div class="mx-auto max-w-[90%] overflow-x-scroll">
-            <h3 class="mb-2">Proizvodi</h3>
-            <va-inner-loading :loading="props.pending">
-                <va-data-table
-                    class=""
-                    striped
-                    :items="props.products"
-                    :columns="columns"
-                    selectable
-                    v-model="selectedItems"
-                    @selection-change="selectHandler"
-                    select-mode="multiple"
-                    :loading="props.pending"
-                >
-                    <template #cell(image)="{ value }"
-                        ><img class="sm:h-[100px] h-auto" :src="value"
-                    /></template>
-                    <template #bodyAppend>
-                        <tr>
-                            <td colspan="8" class="table-example--pagination">
-                                <va-pagination
-                                    :pages="props.totalPages"
-                                    v-model="input.page"
-                                    @update:model-value="handlePageChange"
-                                />
-                            </td>
-                        </tr>
-                    </template> </va-data-table
-            ></va-inner-loading>
-        </div>
-        <div
-            class="sm:ml-16 flex-col sm:flex-row inline-flex flex-column items-center gap-4 justify-start mt-5"
+  <div class="px-4 my-5">
+    <div class="mx-auto max-w-[90%] overflow-x-scroll">
+      <h3 class="mb-2">Proizvodi</h3>
+      <va-inner-loading :loading="props.pending">
+        <va-data-table
+          class=""
+          striped
+          :items="props.products"
+          :columns="columns"
+          selectable
+          v-model="selectedItems"
+          @selection-change="selectHandler"
+          select-mode="multiple"
+          :loading="props.pending"
         >
-            <span>Šifre označenih računa: {{ items.ids.toString() }}</span>
-            <va-button
-                :disabled="items.ids.length !== 0"
-                @click="input.isModalVisible = true"
-                color="success"
-                icon="add_circle"
-                >Dodaj</va-button
-            >
-            <va-button
-                @click="input.isModalVisible = true"
-                :disabled="items.ids.length !== 1"
-                color="warning"
-                icon="update"
-                >Ažuriraj</va-button
-            >
-            <product-form
-                v-if="input.isModalVisible"
-                :isVisible="input.isModalVisible"
-                :product="selectedItems[0]"
-                :predefinedData="predefinedData"
-                @close-modal="input.isModalVisible = false"
-            ></product-form>
-            <va-button
-                :disabled="items.ids.length === 0"
-                @click="removeHandler"
-                color="danger"
-                icon="delete_forever"
-                >Obriši</va-button
-            >
-        </div>
+          <template #cell(image)="{ value }"
+            ><img class="sm:h-[100px] h-auto" :src="value"
+          /></template>
+          <template #bodyAppend>
+            <tr>
+              <td colspan="8" class="table-example--pagination">
+                <va-pagination
+                  :pages="props.totalPages"
+                  v-model="input.page"
+                  @update:model-value="handlePageChange"
+                />
+              </td>
+            </tr>
+          </template> </va-data-table
+      ></va-inner-loading>
     </div>
+    <div
+      class="sm:ml-16 flex-col sm:flex-row inline-flex flex-column items-center gap-4 justify-start mt-5"
+    >
+      <span>Šifre označenih proizvoda: {{ items.ids.toString() }}</span>
+      <va-button
+        :disabled="items.ids.length !== 0"
+        @click="input.isModalVisible = true"
+        color="success"
+        icon="add_circle"
+        >Dodaj</va-button
+      >
+      <va-button
+        @click="input.isModalVisible = true"
+        :disabled="items.ids.length !== 1"
+        color="warning"
+        icon="update"
+        >Ažuriraj</va-button
+      >
+      <product-form
+        v-if="input.isModalVisible"
+        :isVisible="input.isModalVisible"
+        :product="selectedItems[0]"
+        :predefinedData="predefinedData"
+        @close-modal="input.isModalVisible = false"
+      ></product-form>
+      <va-button
+        :disabled="items.ids.length === 0"
+        @click="removeHandler"
+        color="danger"
+        icon="delete_forever"
+        >Obriši</va-button
+      >
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -76,152 +76,152 @@ const selectedItems = ref([]);
 const items = reactive({ ids: [] });
 
 const selectHandler = (prop) => {
-    items.ids = selectedItems.value.map((item) => item['id']);
+  items.ids = selectedItems.value.map((item) => item['id']);
 };
 
 const props = defineProps({
-    products: Array,
-    pending: Boolean,
-    totalPages: Number,
-    currentPage: Number,
+  products: Array,
+  pending: Boolean,
+  totalPages: Number,
+  currentPage: Number,
 });
 
 const input = reactive({
-    searchQuery: '',
-    page: props.currentPage,
-    totalPages: props.totalPages,
-    isModalVisible: false,
+  searchQuery: '',
+  page: props.currentPage,
+  totalPages: props.totalPages,
+  isModalVisible: false,
 });
 
 const predefinedData = reactive({
-    productTypes: [],
-    conditions: [],
-    genres: [],
-    tags: [],
+  productTypes: [],
+  conditions: [],
+  genres: [],
+  tags: [],
 });
 
 const emits = defineEmits(['change_page']);
 
 const handlePageChange = () => {
-    emits('change_page', { newPage: input.page });
+  emits('change_page', { newPage: input.page });
 };
 
 const removeHandler = async () => {
-    const response = useFetch(`${config.API_BASE_URL}/products/deleteProducts`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${userData.token}`,
-        },
-        body: {
-            ids: items.ids,
-        },
-        async onResponseError({ response, error }) {
-            init({
-                title: 'Odjava',
-                position: 'bottom-right',
-                message: 'Greška prilikom brisanja!',
-                color: 'danger',
-                duration: 5000,
-            });
-        },
+  const response = useFetch(`${config.API_BASE_URL}/products/deleteProducts`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${userData.token}`,
+    },
+    body: {
+      ids: items.ids,
+    },
+    async onResponseError({ response, error }) {
+      init({
+        title: 'Odjava',
+        position: 'bottom-right',
+        message: 'Greška prilikom brisanja!',
+        color: 'danger',
+        duration: 5000,
+      });
+    },
+  });
+  if (response.data.value) {
+    init({
+      title: 'Brisanje Proizvoda',
+      position: 'bottom-right',
+      message: 'Proizvodi uspješno obrisani!',
+      color: 'success',
+      duration: 5000,
     });
-    if (response.data.value) {
-        init({
-            title: 'Brisanje Proizvoda',
-            position: 'bottom-right',
-            message: 'Proizvodi uspješno obrisani!',
-            color: 'success',
-            duration: 5000,
-        });
-    }
+  }
 };
 
 const columns = [
-    { key: 'id', name: 'id', label: 'ID' },
-    { key: 'title', name: 'title', label: 'Naslov' },
-    { key: 'initial_price', name: 'initial_price', label: 'Cijena' },
-    { key: 'discount', name: 'discount', label: 'Popust (%)' },
-    { key: 'url', name: 'image', label: 'image' },
+  { key: 'id', name: 'id', label: 'ID' },
+  { key: 'title', name: 'title', label: 'Naslov' },
+  { key: 'initial_price', name: 'initial_price', label: 'Cijena' },
+  { key: 'discount', name: 'discount', label: 'Popust (%)' },
+  { key: 'url', name: 'image', label: 'image' },
 ];
 
 const responseProductTypes = await useFetch(`${config.API_BASE_URL}/productTypes`);
 
 if (!responseProductTypes.error.value) {
-    predefinedData.productTypes = responseProductTypes.data.value;
+  predefinedData.productTypes = responseProductTypes.data.value;
 } else {
-    init({
-        title: 'Dohvaćanje stanja proizvoda',
-        position: 'bottom-right',
-        message: 'Greška prilikom dohvaćanja stanja proizvoda!',
-        color: 'danger',
-        duration: 5000,
-    });
+  init({
+    title: 'Dohvaćanje stanja proizvoda',
+    position: 'bottom-right',
+    message: 'Greška prilikom dohvaćanja stanja proizvoda!',
+    color: 'danger',
+    duration: 5000,
+  });
 }
 
 const responseConditions = await useFetch(`${config.API_BASE_URL}/conditions`, {
-    method: 'GET',
-    initialCache: false,
-    async onResponseError({ response }) {
-        init({
-            title: 'Dohvaćanje stanja proizvoda',
-            position: 'bottom-right',
-            message: 'Greška prilikom dohvaćanja stanja proizvoda!',
-            color: 'danger',
-            duration: 5000,
-        });
-    },
-    async onResponse({ response }) {
-        if (response.status === 200) {
-            predefinedData.conditions = response._data;
-        }
-    },
+  method: 'GET',
+  initialCache: false,
+  async onResponseError({ response }) {
+    init({
+      title: 'Dohvaćanje stanja proizvoda',
+      position: 'bottom-right',
+      message: 'Greška prilikom dohvaćanja stanja proizvoda!',
+      color: 'danger',
+      duration: 5000,
+    });
+  },
+  async onResponse({ response }) {
+    if (response.status === 200) {
+      predefinedData.conditions = response._data;
+    }
+  },
 });
 
 const responseGenres = await useFetch(`${config.API_BASE_URL}/genres`, {
-    method: 'GET',
-    initialCache: false,
-    async onResponseError({ response }) {
-        init({
-            title: 'Dohvaćanje žanrova',
-            position: 'bottom-right',
-            message: 'Greška prilikom dohvaćanja žanrova!',
-            color: 'danger',
-            duration: 5000,
-        });
-    },
-    async onResponse({ response }) {
-        if (response.status === 200) {
-            predefinedData.genres = response._data;
-        }
-    },
+  method: 'GET',
+  initialCache: false,
+  async onResponseError({ response }) {
+    init({
+      title: 'Dohvaćanje žanrova',
+      position: 'bottom-right',
+      message: 'Greška prilikom dohvaćanja žanrova!',
+      color: 'danger',
+      duration: 5000,
+    });
+  },
+  async onResponse({ response }) {
+    if (response.status === 200) {
+      predefinedData.genres = response._data;
+    }
+  },
 });
 
 const responseTags = await useFetch(`${config.API_BASE_URL}/tags`, {
-    method: 'GET',
-    initialCache: false,
-    headers: {
-        Authorization: `Bearer ${userData.token}`,
-        Accept: 'application/json',
-    },
-    async onResponseError({ response }) {
-        init({
-            title: 'Dohvaćanje oznaka',
-            position: 'bottom-right',
-            message: 'Greška prilikom dohvaćanja oznaka!',
-            color: 'danger',
-            duration: 5000,
-        });
-    },
-    async onResponse({ response }) {
-        if (response.status === 200) {
-            predefinedData.tags = response._data;
-        }
-    },
+  method: 'GET',
+  initialCache: false,
+  headers: {
+    Authorization: `Bearer ${userData.token}`,
+    Accept: 'application/json',
+  },
+  async onResponseError({ response }) {
+    init({
+      title: 'Dohvaćanje oznaka',
+      position: 'bottom-right',
+      message: 'Greška prilikom dohvaćanja oznaka!',
+      color: 'danger',
+      duration: 5000,
+    });
+  },
+  async onResponse({ response }) {
+    if (response.status === 200) {
+      predefinedData.tags = response._data;
+    }
+  },
 });
 </script>
 
 <style>
 div {
-    --va-data-table-thead-color: #fff;
+  --va-data-table-thead-color: #fff;
 }
 </style>
