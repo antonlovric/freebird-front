8e0c1abf5c4a15bdef110c6c07b7e8438a090a8d
Anton Lovrić <antonlovri81@gmail.com>
2023-08-04 14:21:06 +0200

    bugfix: logout handler

--- components/MobileNavigation/MobileNavigation.vue
+++ components/MobileNavigation/MobileNavigation.vue
@@ -1,46 +1,46 @@
 <template>
-    <div>
-        <va-sidebar v-if="userData.token == ''" :minimized="isShown.minimized" minimizedWidth="0">
-            <va-sidebar-item v-for="(item, index) in notLoggedUser" :key="index">
-                <nuxt-link :to="item.route">
-                    <va-sidebar-item-content>
-                        <va-icon :name="item.icon" />
-                        <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
-                    </va-sidebar-item-content>
-                </nuxt-link>
-            </va-sidebar-item>
-        </va-sidebar>
-        <va-sidebar
-            v-if="userData.token != ''"
-            :minimized="isShown.minimized"
-            minimizedWidth="0"
-            class="!absolute"
-        >
-            <va-sidebar-item v-for="(item, index) in loggedInUser" :key="index">
-                <nuxt-link v-if="!item.span" :to="item.route">
-                    <va-sidebar-item-content>
-                        <va-icon :name="item.icon" />
-                        <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
-                    </va-sidebar-item-content>
-                </nuxt-link>
-                <span @click="handleLogout" :id="item.id" v-else :to="item.route">
-                    <va-sidebar-item-content>
-                        <va-icon :name="item.icon" />
-                        <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
-                    </va-sidebar-item-content>
-                </span>
-            </va-sidebar-item>
-        </va-sidebar>
-        <va-button
-            id="menu-button"
-            size="large"
-            color="#f97316"
-            icon="menu"
-            class="!fixed !right-6 top-5 z-40"
-            v-model="isShown.minimized"
-            @click="handleMenuClick"
-        ></va-button>
-    </div>
+  <div>
+    <va-sidebar v-if="userData.token == ''" :minimized="isShown.minimized" minimizedWidth="0">
+      <va-sidebar-item v-for="(item, index) in notLoggedUser" :key="index">
+        <nuxt-link :to="item.route">
+          <va-sidebar-item-content>
+            <va-icon :name="item.icon" />
+            <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </nuxt-link>
+      </va-sidebar-item>
+    </va-sidebar>
+    <va-sidebar
+      v-if="userData.token != ''"
+      :minimized="isShown.minimized"
+      minimizedWidth="0"
+      class="!absolute"
+    >
+      <va-sidebar-item v-for="(item, index) in loggedInUser" :key="index">
+        <nuxt-link v-if="!item.span" :to="item.route">
+          <va-sidebar-item-content>
+            <va-icon :name="item.icon" />
+            <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </nuxt-link>
+        <span @click="handleLogout" :id="item.id" v-else :to="item.route">
+          <va-sidebar-item-content>
+            <va-icon :name="item.icon" />
+            <va-sidebar-item-title>{{ item.name }}</va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </span>
+      </va-sidebar-item>
+    </va-sidebar>
+    <va-button
+      id="menu-button"
+      size="large"
+      color="#f97316"
+      icon="menu"
+      class="!fixed !right-6 top-5 z-40"
+      v-model="isShown.minimized"
+      @click="handleMenuClick"
+    ></va-button>
+  </div>
 </template>
 
 <script setup>
@@ -55,68 +55,70 @@ const { init } = useToast();
 const config = useRuntimeConfig();
 
 const handleMenuClick = () => {
-    isShown.minimized = !isShown.minimized;
+  isShown.minimized = !isShown.minimized;
 };
 
 const notLoggedUser = ref([
-    { name: 'Naslovnica', icon: 'home', route: '/' },
-    { name: 'Katalog', icon: 'library_music', route: '/catalogue' },
-    { name: 'Novosti', icon: 'feed', route: '/blogPosts' },
-    { name: 'Košarica', icon: 'shopping_cart', route: '/cart' },
-    { name: 'Registracija', icon: 'how_to_reg', route: '/registration' },
-    { name: 'Prijava', icon: 'login', route: '/login' },
+  { name: 'Naslovnica', icon: 'home', route: '/' },
+  { name: 'Katalog', icon: 'library_music', route: '/catalogue' },
+  { name: 'Novosti', icon: 'feed', route: '/blogPosts' },
+  { name: 'Košarica', icon: 'shopping_cart', route: '/cart' },
+  { name: 'Registracija', icon: 'how_to_reg', route: '/registration' },
+  { name: 'Prijava', icon: 'login', route: '/login' },
 ]);
 
 const loggedInUser = ref([
-    { name: 'Naslovnica', icon: 'home', route: '/' },
-    { name: 'Katalog', icon: 'library_music', route: '/catalogue' },
-    { name: 'Novosti', icon: 'feed', route: '/blogPosts' },
-    { name: 'Profil', icon: 'account_circle', route: '/profile' },
-    { name: 'Košarica', icon: 'shopping_cart', route: '/cart' },
-    { name: 'Odjava', icon: 'logout', span: 'true', id: 'logout' },
+  { name: 'Naslovnica', icon: 'home', route: '/' },
+  { name: 'Katalog', icon: 'library_music', route: '/catalogue' },
+  { name: 'Novosti', icon: 'feed', route: '/blogPosts' },
+  { name: 'Profil', icon: 'account_circle', route: '/profile' },
+  { name: 'Košarica', icon: 'shopping_cart', route: '/cart' },
+  { name: 'Odjava', icon: 'logout', span: 'true', id: 'logout' },
 ]);
 
 const handleLogout = async (event) => {
-    init({
+  init({
+    title: 'Odjava',
+    position: 'bottom-right',
+    message: 'Pričekajte...',
+    duration: 5000,
+  });
+  const response = await useFetch(`${config.public.API_BASE_URL}/auth/logout`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${userData.token}`,
+    },
+    initialCache: false,
+    async onResponseError({ response }) {
+      init({
         title: 'Odjava',
         position: 'bottom-right',
-        message: 'Pričekajte...',
+        message: 'Greška prilikom odjave!',
+        color: 'danger',
         duration: 5000,
+      });
+    },
+    async onResponse() {
+      userData.resetStore();
+      cartData.clearCart();
+      const rememberCookie = useCookie('remember_token');
+      rememberCookie.value = null;
+    },
+  });
+  if (!response.error?.value) {
+    init({
+      title: 'Odjava',
+      position: 'bottom-right',
+      message: 'Uspješna odjava!',
+      color: 'success',
+      duration: 5000,
     });
-    const response = await useFetch(`${config.public.API_BASE_URL}/auth/logout`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${userData.token}`,
-        },
-        initialCache: false,
-        async onResponseError({ response }) {
-            init({
-                title: 'Odjava',
-                position: 'bottom-right',
-                message: 'Greška prilikom odjave!',
-                color: 'danger',
-                duration: 5000,
-            });
-        },
-    });
-    if (!response.error?.value) {
-        userData.resetStore();
-        cartData.clearCart();
-        const rememberCookie = useCookie('remember_token');
-        rememberCookie.value = null;
-        init({
-            title: 'Odjava',
-            position: 'bottom-right',
-            message: 'Uspješna odjava!',
-            color: 'success',
-            duration: 5000,
-        });
-    }
+  }
 };
 </script>
 
 <style scoped>
 .va-sidebar {
-    position: fixed !important;
+  position: fixed !important;
 }
 </style>
